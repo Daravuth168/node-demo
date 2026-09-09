6187d4a651d3b0d3335dc49f4ff6c61519bdc264
Chhean Daravuth <daravuth.chhean@lhl.com>
2026-09-09 15:39:02 +0700

    Update application version

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello HFC Cambodia V1.0");
+    res.send("Hello HFC Cambodia V2.0");
 });
 
 app.get("/health", (req, res) => {
